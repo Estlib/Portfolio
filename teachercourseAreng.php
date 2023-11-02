0fcb370869264b13a8480c8ca4cc59ca356bdcc0
Estlib <75094683+Estlib@users.noreply.github.com>
2023-11-02 15:44:30 +0200

    added page

---
+++ teachercourseAreng.php
@@ -0,0 +1,25 @@
+<?php require("menu.php"); ?>
+<main class="main">
+    <h1 id="darkboxed">Jüri Vaitmaa</h1>
+    <br>
+    <p style="text-align: center;" id="darkboxed">This Website
+        <br>
+        No content.
+        <br>
+        Empty unordered list:
+        <br>
+        <ul>
+            <li>List item 1</li>
+            <li>List item 2</li>
+            <li>List item 3</li>
+            <li>List item 4</li>
+            <li>List item 5</li>
+            <li>List item 6</li>
+            <li>List item 7</li>
+        </ul>
+    </p>
+</main>
+</div>
+<!-- <?php require("foot.php"); ?> -->
+</body>
+</html>
